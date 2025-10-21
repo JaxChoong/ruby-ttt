68329b58350ad6b45514d57380662f0787befd41
Jax Choong <choongjax@gmail.com>
2025-10-22 00:00:05 +0800

    style: renamed the next move prompt

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require_relative "lib/grid"
 require_relative "lib/player"
 
 player_array = []
-something = Grid.new
+game = Grid.new
 (1..2).each do |i|
     # get player 1 and 2's icons
     print("Player #{i} Select Icon: 'X' or 'O': ")
@@ -13,13 +13,13 @@ end
 
 x = 0
 
-something.print_grid
+game.print_grid
 begin
     # cycles between player 1 and player 2
-    print "Player #{player_array[x].number} next move: "
+    print "Player #{player_array[x].number} enter grid position for next move: "
     move = gets.to_i
-    something[move] = player_array[x].icon
+    game[move] = player_array[x].icon
     x = (x+1) % 2
-    something.print_grid
-end while !something.game_end?
+    game.print_grid
+end while !game.game_end?
 puts "Player #{x} wins!"
