e4627be627c4da1f1119feaae736d33c8242005a
Jax Choong <choongjax@gmail.com>
2025-10-21 23:41:29 +0800

    feat: now cycles between player moves

--- main.rb
+++ main.rb
@@ -2,13 +2,19 @@ require_relative "lib/grid"
 require_relative "lib/player"
 
 player_array = []
+something = Grid.new
 (1..2).each do |i|
+    # get player 1 and 2's icons
     print("Player #{i} Select Icon: 'X' or 'O': ")
     symbol = gets.chomp.upcase
     player = Player.new(i,symbol)
     player_array << player
 end
-something = Grid.new
 something.print_grid
-
+x = 0
+(0...7).each do |i|
+    # cycles between player 1 and player 2
+    print "Player #{player_array[x].number} next move: \n"
+    x = (x+1) % 2
+end
 p player_array
