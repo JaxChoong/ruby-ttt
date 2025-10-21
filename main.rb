1938dee83d9d2387d74028bf0662d03dfa92f97f
Jax Choong <choongjax@gmail.com>
2025-10-21 23:32:37 +0800

    feat: now gets player Icons and put them into an array

--- main.rb
+++ main.rb
@@ -1,4 +1,14 @@
 require_relative "lib/grid"
+require_relative "lib/player"
 
+player_array = []
+(1..2).each do |i|
+    print("Player #{i} Select Icon: 'X' or 'O': ")
+    symbol = gets.chomp.upcase
+    player = Player.new(i,symbol)
+    player_array << player
+end
 something = Grid.new
 something.print_grid
+
+p player_array
